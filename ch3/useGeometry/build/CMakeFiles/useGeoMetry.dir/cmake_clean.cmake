ef417616da548970b19e5d3f0e4f19f485bd2e65
wamwangyabin <yabinwango@gmail.com>
2018-09-29 14:04:44 +0800

    add useGeometry

---
+++ ch3/useGeometry/build/CMakeFiles/useGeoMetry.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/useGeoMetry.dir/useGeometry.cpp.o"
+  "useGeoMetry.pdb"
+  "useGeoMetry"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/useGeoMetry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
