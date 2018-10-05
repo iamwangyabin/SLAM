d615c6aec3d78fd6dce7c208b507f3b4e3eddc0d
wamwangyabin <yabinwango@gmail.com>
2018-10-05 19:01:48 +0800

    add ch8

---
+++ ch8/useLK/build/CMakeFiles/useLk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/useLk.dir/useLK.o"
+  "useLk.pdb"
+  "useLk"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/useLk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
